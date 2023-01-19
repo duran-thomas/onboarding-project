3d7bf5d788f88b02b36ced59fef169c9c7c74e60
Duran Thomas <22084776+duran-thomas@users.noreply.github.com>
2023-01-19 15:26:46 -0500

    feat:verify name and add entry to guest book

---
+++ tests/account_test.js
@@ -0,0 +1,12 @@
+Feature('Guestbook');
+    const r_ship = Math.floor(Math.random() * 5) + 1
+Scenario('verify username and add entry to guestbook',  ({ I }) => {
+    I.amOnPage('/us/obituaries/chicagotribune/name/virginia-gruchalski-obituary?pid=196167379');
+    I.see('Virginia','.Heading-sc-4h3nqe-0')
+    I.scrollIntoView('#guestbook')
+    I.fillField('textarea[name=_GuestbookForm_Message]', 'Sorry for your loss')
+    I.click(`div.Box-sc-ucqo0b-0.jjAWvY > div > div:nth-child(${r_ship}) > button`)
+    I.fillField('input[name=_GuestbookForm_From]', 'Duran')
+    I.fillField('input[name=_GuestbookForm_Email]', 'dthomas@qualityworkscg.com')
+    I.click('div.Box-sc-ucqo0b-0.cbNzut > div.Box-sc-ucqo0b-0.Flex-sc-d1l2vy-0.fsrkdX > button')
+});
