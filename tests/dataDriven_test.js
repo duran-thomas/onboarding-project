266fbca9cf1617fd823acce9ccfd63c59dc794b5
Duran Thomas <22084776+duran-thomas@users.noreply.github.com>
2023-01-31 10:23:35 -0500

    add comments to the test file

--- tests/dataDriven_test.js
+++ tests/dataDriven_test.js
@@ -8,20 +8,27 @@ data.add([
   "404",
 ]);
 
-var assert = require("assert");
+const assert = require("assert");
+
 Feature("Data Driven");
 
 Scenario("I can verify the URL returns a status code of 404", async ({ I }) => {
+  // Given that I try to access an incorrect URL
+  // When I try to access an incorrect URL
   let res = await I.sendGetRequest(
     "/us/obituaries/chicagotribune/name/virginia-gruchalski-obituary?pid=098734232"
   );
+  // Then I should receive a 404 status code from the API
   assert.equal(res.status, 404);
 });
 
 Data(data).Scenario(
   "I can verify the URL status code using a Data Table",
   async ({ I, current }) => {
+    // Given that I try to access a URL
+    // When I try to access a  URL
     let res = await I.sendGetRequest(current.url);
+    // Then I should receive a 404 status code if the URL is correct and 404 if incorrect
     assert.equal(res.status, current.statusCode);
   }
 );
